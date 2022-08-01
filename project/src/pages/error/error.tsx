6b2c5881dc52b376d79489e1465f8727afa9960d
NatalyLu <natulya.lukashina@bk.ru>
2022-08-01 20:15:28 +0300

    new page - 404

---
+++ project/src/pages/error/error.tsx
@@ -0,0 +1,10 @@
+function Error(): JSX.Element {
+  return (
+    <section>
+        <h1>404: Страница не найдена</h1>
+        <a href="/"> На главную</a>
+    </section>
+  );
+}
+
+export default Error;
