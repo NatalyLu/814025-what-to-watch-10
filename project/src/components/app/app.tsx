933085f734efac7d6eb89b7e31182753bafde212
NatalyLu <natulya.lukashina@bk.ru>
2022-08-01 20:15:50 +0300

    added routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
+import SignIn from '../../pages/sign-in/sign-in';
+import MyList from '../../pages/my-list/my-list';
+import {default as OneFilm} from '../../pages/film/film';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player/player';
 import {PromoFilm} from '../../types/types';
 import {Film} from '../../types/types';
 
@@ -9,7 +16,39 @@ type AppProps = {
 
 function App(props: AppProps): JSX.Element {
   return (
-    <Main promoFilm={props.promoFilm} films={props.films} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main promoFilm={props.promoFilm} films={props.films} />}
+        />
+
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList films={props.films} />}
+        />
+
+        <Route
+          path={AppRoute.Film}
+          element={<OneFilm films={props.films} />}
+        />
+
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
