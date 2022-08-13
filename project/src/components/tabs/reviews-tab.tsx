a2a3dc626eed02c2f2e5cb38ca29b009c2fb224c
NatalyLu <natulya.lukashina@bk.ru>
2022-08-13 11:49:11 +0300

    fixed the linter's error

--- project/src/components/tabs/reviews-tab.tsx
+++ project/src/components/tabs/reviews-tab.tsx
@@ -12,24 +12,22 @@ function ReviewsTab(): JSX.Element {
   const firstColumn = filmReviews.slice(0, IndexOfArrayCenter);
   const secondColumn = filmReviews.slice(IndexOfArrayCenter);
 
-  const getReviews = (array: TabsReview[]): JSX.Element[] => {
-    return(
-      array.map((item) =>
-        (
-          <div className="review" key={item.author}>
-            <blockquote className="review__quote">
-              <p className="review__text">{item.description}</p>
-              <footer className="review__details">
-                <cite className="review__author">{item.author}</cite>
-                <time className="review__date" dateTime={item.dateTime}>{item.date}</time>
-              </footer>
-            </blockquote>
-            <div className="review__rating">{item.rating}</div>
-          </div>
-        )
+  const getReviews = (array: TabsReview[]): JSX.Element[] => (
+    array.map((item) =>
+      (
+        <div className="review" key={item.author}>
+          <blockquote className="review__quote">
+            <p className="review__text">{item.description}</p>
+            <footer className="review__details">
+              <cite className="review__author">{item.author}</cite>
+              <time className="review__date" dateTime={item.dateTime}>{item.date}</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">{item.rating}</div>
+        </div>
       )
-    );
-  };
+    )
+  );
 
   return (
     <div className="film-card__reviews film-card__row">
