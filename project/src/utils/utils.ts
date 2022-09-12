4bd4d35d8dc60db08efe06594c451ac00336f856
NatalyLu <natulya.lukashina@bk.ru>
2022-09-12 23:58:59 +0300

    fixed the getDay function

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -3,26 +3,13 @@ import {Films} from '../types/types';
 
 export const checkId = (films: Films, id: number) => films.some((film) => film.id === id);
 
-export const getDate = (date: Date) => {
-  const monthMap = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const day = (date.getDay() + 1).toString().padStart(2, '0');
-  const month = monthMap[date.getMonth()];
-  const year = date.getFullYear();
-  return `${month} ${day}, ${year}`;
-};
+export const getDate = (date: Date) => (
+  new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  }).format(date)
+);
 
 export const getStarsCount = (rating: number) => {
   if (rating < 3){
